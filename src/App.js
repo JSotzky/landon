9449ca327920176089d5dc7c3ca5be7f68e5afd5
Jason Sotzky <Jasonsotzky@gmail.com>
2022-05-06 21:55:02 -0400

    added react components, added json for data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn in less than a day!
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
